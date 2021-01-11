08ebc7777f6ddf880d038272eb7ab9c4e40080ed
Jesus Zazueta <jjzazuet@vacco.io>
2021-01-10 19:57:56 -0500

    - Initial commit.

---
+++ README.md
@@ -0,0 +1,3 @@
+# volach
+
+[volach](https://en.wikipedia.org/wiki/Valac) is a minimal audio fingerprinting library.
---
+++ build.gradle.kts
@@ -0,0 +1,27 @@
+plugins { id("io.vacco.common-build") version "0.5.3" }
+
+repositories {
+  maven {
+    url = uri("https://dl.bintray.com/vaccovecrana/vacco-oss")
+  }
+}
+
+group = "io.vacco.volach"
+version = "0.1.0"
+
+configure<io.vacco.common.CbPluginProfileExtension> {
+  addJ8Spec()
+  addPmd()
+  addSpotBugs()
+  addClasspathHell()
+  setPublishingUrlTransform { repo -> "${repo.url}/${project.name}" }
+  sharedLibrary()
+}
+
+val api by configurations
+
+dependencies {
+  api("io.vacco.jaad:jaad:0.8.7")
+  api("io.vacco.jlame:jlame:3.100.0")
+  api("io.vacco.joggvorbis:joggvorbis:1.3.1")
+}
