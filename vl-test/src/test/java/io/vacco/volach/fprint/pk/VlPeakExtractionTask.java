6a9a0ef9b3b2d55e71c2f2e6fd8f0606e971ce52
Jesus Zazueta <jjzazuet@vacco.io>
2021-03-21 13:24:39 -0400

    - Initial reference dataset.

--- vl-test/src/test/java/io/vacco/volach/fprint/pk/VlPeakExtractionTask.java
+++ vl-test/src/test/java/io/vacco/volach/fprint/pk/VlPeakExtractionTask.java
@@ -18,6 +18,7 @@ public class VlPeakExtractionTask {
       VlTrainingDataSet.VlSampleSource sSrc = mapper.readValue(spectrumFile, VlTrainingDataSet.VlSampleSource.class);
       for (VlTrainingDataSet.VlAnchorPoint anchor : tSrc.anchors) {
         float[][] region = new float[trainingRegionSize][trainingRegionSize];
+        System.out.printf("Extracting anchor point: [%s, %s]%n", anchor.x, anchor.y);
         regionSquare(sSrc.spectrum, anchor.x, anchor.y, region);
         anchor.region = region;
       }
